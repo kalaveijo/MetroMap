5e6ca02b7edcc39a84f2b2d44fd23ebb3c798f6d
kalaveijo <kalaveijo@gmail.com>
2013-10-28 16:20:40 +0200

    Added better rubber string effect on flings
    still little buggy

--- src/metropolia/project/metromap/Floor.java
+++ src/metropolia/project/metromap/Floor.java
@@ -5,49 +5,62 @@ import android.graphics.Point;
 
 public class Floor {
 
-	public final int SPEED = 1; //speed which moves floor towards target
-	
+	public final int SPEED = 1; // speed which moves floor towards target
+
 	public Bitmap picture;
 	private Point location;
 	private Point target;
-	
-	public Floor(Point loc){
+
+	public Floor(Point loc) {
 		this.location = loc;
 		this.target = loc;
 	}
-	
-	
+
 	// called by tick() to move each floor object
-	public void move(){
-		if(target.y > location.y){
-			location.y = location.y + SPEED;
-		}else if(target.y < location.y){
-			location.y = location.y - SPEED;
+	public void move() {
+		if (target.y > location.y) {
+
+			if (target.y - location.y > 60) {
+				location.y = location.y + SPEED + 6;
+			} else if (target.y - location.y > 20) {
+				location.y = location.y + SPEED + 3;
+			} else {
+				location.y = location.y + SPEED;
+			}
+
+		} else if (target.y < location.y) {
+			if (location.y - target.y > 60) {
+				location.y = location.y - SPEED - 6;
+			} else if (location.y - target.y > 20) {
+				location.y = location.y - SPEED - 3;
+			} else {
+				location.y = location.y - SPEED;
+			}
 		}
 	}
-	
+
 	/*
 	 * Getters/Setters
 	 */
-	
-	public void setLocation(Point newLoc){
+
+	public void setLocation(Point newLoc) {
 		this.location = newLoc;
 	}
-	
-	public Point getLocation(){
+
+	public Point getLocation() {
 		return this.location;
 	}
-	
-	public void setTarget(Point newLoc){
+
+	public void setTarget(Point newLoc) {
 		this.target = newLoc;
 	}
-	
-	public Point getTarget(){
+
+	public Point getTarget() {
 		return this.target;
 	}
-	
-	public Bitmap getPicture(){
+
+	public Bitmap getPicture() {
 		return this.picture;
 	}
-	
+
 }
--- src/metropolia/project/metromap/FloorMapView.java
+++ src/metropolia/project/metromap/FloorMapView.java
@@ -2,8 +2,8 @@ package metropolia.project.metromap;
 
 import java.util.ArrayList;
 
-import metropolia.project.utility.MetroMapSurfaceView;
 import metropolia.project.utility.MetroMapEvent;
+import metropolia.project.utility.MetroMapSurfaceView;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,22 +14,24 @@ import android.graphics.Point;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.View.OnTouchListener;
 
 /*
  * Handles painting of images, animating and touch listeners
  */
-public class FloorMapView extends MetroMapSurfaceView implements OnTouchListener,
-		SurfaceHolder.Callback {
+public class FloorMapView extends MetroMapSurfaceView implements
+		OnTouchListener, SurfaceHolder.Callback {
 
-	private final int TOLERANCE = 50; //used to check if user has touched close enough to floor maps
+	private final int TOLERANCE = 50; // used to check if user has touched close
+										// enough to floor maps
 	private final boolean DEBUG = true; // enables debug data to this view
-	
+	private final int Y_BOUNDARY_TOP = 0;
+	private final int Y_BOUNDARY_BOTTOM = 1200;
+
 	private Paint mPaint;
 	private Floor[] floor = new Floor[4];
 	private GestureDetector mDetector;
@@ -42,10 +44,10 @@ public class FloorMapView extends MetroMapSurfaceView implements OnTouchListener
 		super(context, attrs);
 		this.context = context;
 		initBall();
-		
+
 	}
 
-	//Initializes animation thread and canvas paints
+	// Initializes animation thread and canvas paints
 	private void initBall() {
 		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG); // reduce the
 		mPaint.setColor(Color.BLACK); // jaggedness of lines in graphics
@@ -54,47 +56,43 @@ public class FloorMapView extends MetroMapSurfaceView implements OnTouchListener
 
 		// haha en tee tätä oikein hv
 		addFloorMaps();
-		
+
 		getHolder().addCallback(this);
 		aThread = new AnimationThread(getHolder(), this);
-		
+
 		this.setOnTouchListener(this);
-		
+
 		mDetector = new GestureDetector(getContext(),
 				new SimpleOnGestureListener() {
-			
-					//check if user wants to select correct floor
+
+					// check if user wants to select correct floor
 					public boolean onSingleTapConfirmed(MotionEvent e) {
 						alMMEvent.clear();
-						MetroMapEvent mme = new MetroMapEvent((int) e.getX(), (int) e.getY());
+						MetroMapEvent mme = new MetroMapEvent((int) e.getX(),
+								(int) e.getY());
 						alMMEvent.add(mme);
 						checkIfMapPress(mme);
 						return true;
 					}
-					
+
 					/*
-					 * (non-Javadoc)
-					 * Used to get motion from flings and assigning new target locations for floors
-					 * @see android.view.GestureDetector.SimpleOnGestureListener#onFling(android.view.MotionEvent, android.view.MotionEvent, float, float)
+					 * (non-Javadoc) Used to get motion from flings and
+					 * assigning new target locations for floors
+					 * 
+					 * @see
+					 * android.view.GestureDetector.SimpleOnGestureListener#
+					 * onFling(android.view.MotionEvent,
+					 * android.view.MotionEvent, float, float)
 					 */
-					public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY){
-						if(velocityY > 0){
-							
-							for(Floor f : floor){
-								f.setTarget(new Point(f.getLocation().x, (f.getLocation().y + 80)));
-							}
-							
-						}else if(velocityY < 0){
-							
-							for(Floor f : floor){
-								f.setTarget(new Point(f.getLocation().x, (f.getLocation().y - 80)));
-							}
-						}
-						
+					public boolean onFling(MotionEvent e1, MotionEvent e2,
+							float velocityX, float velocityY) {
+
+						handleFling(velocityY);
+
 						invalidate();
 						return true;
 					}
-					
+
 				});
 
 	}
@@ -104,31 +102,36 @@ public class FloorMapView extends MetroMapSurfaceView implements OnTouchListener
 	 */
 	public void doDraw(Canvas canvas, MetroMapEvent e) {
 		canvas.drawColor(Color.WHITE);
-		
-		if(DEBUG){
-			canvas.drawText(String.valueOf("ms: " + e.getTime()), 20, 20, mPaint);
-			canvas.drawText(String.valueOf("x: " + e.getLocation().x), 20, 40, mPaint);
-			canvas.drawText(String.valueOf("y: " + e.getLocation().y), 20, 60, mPaint);
+
+		if (DEBUG) {
+			canvas.drawText(String.valueOf("ms: " + e.getTime()), 20, 20,
+					mPaint);
+			canvas.drawText(String.valueOf("x: " + e.getLocation().x), 20, 40,
+					mPaint);
+			canvas.drawText(String.valueOf("y: " + e.getLocation().y), 20, 60,
+					mPaint);
 		}
-		
-		for(Floor f : floor){
-			canvas.drawBitmap(f.getPicture(), f.getLocation().x, f.getLocation().y, mPaint);
-		}	
+
+		for (Floor f : floor) {
+			canvas.drawBitmap(f.getPicture(), f.getLocation().x,
+					f.getLocation().y, mPaint);
+		}
 	}
 
 	public void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		canvas.drawColor(Color.WHITE);
-		for(Floor f : floor){
-			canvas.drawBitmap(f.getPicture(),f.getLocation().x, f.getLocation().y, mPaint);
-		}	
+		for (Floor f : floor) {
+			canvas.drawBitmap(f.getPicture(), f.getLocation().x,
+					f.getLocation().y, mPaint);
+		}
 	}
 
 	/*
 	 * called once in each animation cycle
 	 */
 	public void tick() {
-		for(Floor f : floor){
+		for (Floor f : floor) {
 			f.move();
 		}
 	}
@@ -165,7 +168,9 @@ public class FloorMapView extends MetroMapSurfaceView implements OnTouchListener
 
 	/*
 	 * (non-Javadoc)
-	 * @see android.view.View.OnTouchListener#onTouch(android.view.View, android.view.MotionEvent)
+	 * 
+	 * @see android.view.View.OnTouchListener#onTouch(android.view.View,
+	 * android.view.MotionEvent)
 	 */
 	public boolean onTouch(View v, MotionEvent event) {
 		mDetector.onTouchEvent(event);
@@ -175,52 +180,102 @@ public class FloorMapView extends MetroMapSurfaceView implements OnTouchListener
 		}
 		return true;
 	}
-	
-	
+
 	/*
 	 * Initializes Floor objects with places and relates .png files
 	 */
-	public void addFloorMaps(){
-		
-		for(int i = 0; i < 4; i++){
-			floor[i] = new Floor(new Point(100, (450-(150*i))));
+	public void addFloorMaps() {
+
+		for (int i = 0; i < 4; i++) {
+			floor[i] = new Floor(new Point(100, (450 - (150 * i))));
 		}
-		
+
 		floor[3].picture = BitmapFactory.decodeResource(getResources(),
 				R.drawable.floor3);
-		floor[3].picture = Bitmap.createScaledBitmap(floor[3].picture, 150, 150, true);
-		
+		floor[3].picture = Bitmap.createScaledBitmap(floor[3].picture, 150,
+				150, true);
+
 		floor[2].picture = BitmapFactory.decodeResource(getResources(),
 				R.drawable.floor2);
-		floor[2].picture = Bitmap.createScaledBitmap(floor[2].picture, 150, 150, true);
-		
+		floor[2].picture = Bitmap.createScaledBitmap(floor[2].picture, 150,
+				150, true);
+
 		floor[1].picture = BitmapFactory.decodeResource(getResources(),
 				R.drawable.floor1);
-		floor[1].picture = Bitmap.createScaledBitmap(floor[1].picture, 150, 150, true);
-		
+		floor[1].picture = Bitmap.createScaledBitmap(floor[1].picture, 150,
+				150, true);
+
 		floor[0].picture = BitmapFactory.decodeResource(getResources(),
 				R.drawable.floor0);
-		floor[0].picture = Bitmap.createScaledBitmap(floor[0].picture, 150, 150, true);
+		floor[0].picture = Bitmap.createScaledBitmap(floor[0].picture, 150,
+				150, true);
 	}
-	
-	//called by animation thread to get happened events
-	public ArrayList<MetroMapEvent> getEvents(){
+
+	// called by animation thread to get happened events
+	public ArrayList<MetroMapEvent> getEvents() {
 		return this.alMMEvent;
 	}
-	
+
 	/*
 	 * called by animation thread to check if user has pressed any floor icons
 	 */
-	public void checkIfMapPress(MetroMapEvent e){
-		for(Floor f : floor){
+	public void checkIfMapPress(MetroMapEvent e) {
+		for (Floor f : floor) {
 
-			if(Math.abs(e.getLocation().x - f.getLocation().x) < TOLERANCE ){
-				if(Math.abs(e.getLocation().y - f.getLocation().y) < TOLERANCE ){
+			if (Math.abs(e.getLocation().x - f.getLocation().x) < TOLERANCE) {
+				if (Math.abs(e.getLocation().y - f.getLocation().y) < TOLERANCE) {
 					MainActivity ma = (MainActivity) context;
 					ma.changeFragment(new SingleFloor());
 				}
 			}
 		}
 	}
-	
+
+	/*
+	 * Handles fling movement
+	 */
+	public void handleFling(float velocityY) {
+
+		// when moving down
+		if (velocityY > 0) {
+			int velY = (int) velocityY / 10;
+			int tempY = velY;
+
+			for (Floor f : floor) {
+				if (f.getLocation().y + velY > Y_BOUNDARY_BOTTOM) {
+					tempY = -Y_BOUNDARY_BOTTOM + f.getLocation().y;
+					// Y_BOUNDARY_BOTTOM means screensize, should get this
+					// dynamically but ->
+					// LAZY. This checks if our fling would go totally overboard
+				}
+			}
+			velY = tempY;
+
+			for (Floor f : floor) {
+				f.setTarget(new Point(f.getLocation().x,
+						(f.getLocation().y + velY)));
+			}// for
+
+			// when moving up
+		} else if (velocityY < 0) {
+			int velY = (int) velocityY / 10;
+			int tempY = velY;
+
+			for (Floor f : floor) {
+				if (f.getLocation().y - velY < Y_BOUNDARY_TOP) {
+					tempY = -Y_BOUNDARY_TOP - f.getLocation().y;
+					// 800 means screensize, should get this dynamically but ->
+					// LAZY. This checks if our fling would go totally overboard
+				}
+			}
+
+			velY = tempY;
+
+			for (Floor f : floor) {
+				f.setTarget(new Point(f.getLocation().x,
+						(f.getLocation().y + velY)));
+			}// for
+		}// if
+
+	}// handleFling
 }
